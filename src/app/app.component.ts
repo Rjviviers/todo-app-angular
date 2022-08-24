5b8974f4cc5f2e6a0f6d391c1f5d7362471ffd7c
Ruan Viviers <rjviviers@gmail.com>
2022-08-24 14:03:49 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
+    //state
+    todos : string[] = [];
+    todoText  = '';
+    // lifecycle
+    ngOnInit() {
+        const existingTodos = localStorage.getItem('todos');
+        if (existingTodos) {
+            this.todos = JSON.parse(existingTodos);
+        }
+
+    }
+
+    //events
+    addTodo() {
+        this.todos.push(this.todoText);
+        localStorage.setItem('todos', JSON.stringify(this.todos));
+        this.todoText = '';
+    }
+
 }
